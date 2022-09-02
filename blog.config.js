3d4f1e307b27c1d5e2899c62148501177ed9a238
sonderr <38247842+sonderrj@users.noreply.github.com>
2022-09-02 17:58:06 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
-  title: 'CRAIGARY',
-  author: 'Craig Hart',
+  title: 'SONDERR‘S HOME',
+  author: 'SONDERR',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
